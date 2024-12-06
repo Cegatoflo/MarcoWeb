3c0a4cbcc13f274d1ae212005f1ba596f0bb12be
Jmar20 <u22238783@utp.edu.pe>
2024-12-06 15:33:59 -0500

    Productos Delete

--- FrontEnd/src/front/Screens/Dashboard.jsx
+++ FrontEnd/src/front/Screens/Dashboard.jsx
@@ -2,7 +2,6 @@ import '../Styles/SDashboard.css';
 import '../Styles/inter.css';
 
 import { Navbar } from '../Components/Navbar';
-import { Productview } from './SubScreens/Productview';
 import { Customerview } from './SubScreens/Customerview';
 import { Orderviews } from './SubScreens/Orderviews';
 import { Inventoryview } from './SubScreens/Inventoryview';
@@ -16,7 +15,6 @@ export function Dashboard({ rightType }) {
             <div className='general-container'>
                 <Navbar />
                 <div className='Right'>
-                    {rightType === 'productos' && <Productview />}
                     {rightType === 'clientes' && <Customerview />}
                     {rightType === 'ordenes' && <Orderviews />}
                     {rightType === 'inventario' && <Inventoryview/>}
