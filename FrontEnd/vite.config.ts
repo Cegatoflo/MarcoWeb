77febc390a7fdd3123813b70045e1492f242cc54
Cesar Torres <cesargabrieltorresflorez@gmail.com>
2024-12-06 20:28:35 -0500

    ga
    1

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: './src/front', // La carpeta raíz de tu proyecto
   plugins: [react()],
-  server: {
-    port: 8080, // Puerto que usará Vite en producción
-    host: '0.0.0.0', // Escuchar en todas las interfaces de red
-  },
-});
+})
