ef956eace8de296e366900fd483caf72c4f7d6a5
Cesar Torres <cesargabrieltorresflorez@gmail.com>
2024-12-11 17:24:55 -0500

    Revert "HOLA"
    This reverts commit 6ae4ea1734332f64b96976f7ad749eb7b25b65f8.

--- FrontEnd/src/front/Components/Navbar.jsx
+++ FrontEnd/src/front/Components/Navbar.jsx
@@ -5,16 +5,19 @@ import { useNavigate } from 'react-router-dom';
 const navItems = ["Home", "Clientes", "Ordenes", "Inventario", "Informes"];
 
 export function Navbar() {
-    const navigate = useNavigate(); 
+    const navigate = useNavigate(); // Inicializa el hook para la navegación
 
     const handleLogout = async () => {
         try {
+            // Realiza la solicitud de logout al backend
             await axios.post('https://pyfjs.onrender.com/api/auth/logout', {}, {
                 withCredentials: true
             });
 
+            // Elimina el token de la cookie
             document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=pyfjs.onrender.com; Secure; HttpOnly; SameSite=None";
 
+            // Redirige al usuario a la página de login
             navigate('/login');
         } catch (error) {
             console.error('Error al cerrar sesión:', error.response?.data?.message || 'Error');
